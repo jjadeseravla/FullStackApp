f2eee2a06641e5d0f512f9abfbf72aa02ba27c7f
jjadeseravla <jadealvares@hotmail.co.uk>
2020-02-11 11:39:59 +0000

    GET request from front to backend (remember to start back end server with front end!)

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import './App.css';
+import { getAllStudents } from './client';
 
+//takes response and grabs json inside of it
 function App() {
+
+  getAllStudents()
+      .then(res => res.text()
+      .then(students => {
+    console.log(students);
+  }))
+
   return (
    <h1>hello world springboot and react</h1>
   )
