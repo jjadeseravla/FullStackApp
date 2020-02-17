a8e63e06ddc818773c17762125b44241579cade5
jjadeseravla <jadealvares@hotmail.co.uk>
2020-02-17 15:05:11 +0000

    removed dataSource folder and file because Hikari is default on latest version of Springboot.

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,48 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import { getAllStudents } from './client';
-
 //takes response and grabs json inside of it
 function App() {
+    const [state, setState] = useState({students:[]});
+    useEffect(() => {
+        fetchStudents();
+    }, [])
+    function fetchStudents() {
+        getAllStudents()
+            .then(res => res.json())
+            .then(students => {
+                setState({
+                    students: students
+                }, ()=>{
+                    console.log(state.students[0]['firstName']);
+                });
+                console.log(typeof students)
+                // console.log(state.students[0]['firstName']);
+            })
+    }
 
-  const [state, setState] = useState([]);
-
-  useEffect(() => {
-    fetchStudents();
-  }, [])
-
-  function fetchStudents() {
-     getAllStudents()
-      .then(res => res.text()
-      .then(students => {
-        setState({
-          students: students
-        });
-        console.log(students)
-    }))
-  }
-
-  //this.students = this.students.bind(this)
-
-  // const { students  = this.props}
-
-  // if (students && students.length) {
-  //   students.map((student, id) => {
-      return (
-      <div>
-        <h1>Students</h1>
-        {/*<div id={id}>*/}
-        <div>
-          <h2>
-            <p>{state.students}</p>
-            {/*<p>{student.firstName}</p>*/}
-            {/*<p>{student.secondName}</p>*/}
-            {/*<p>{student.gender}</p>*/}
-            {/*<p>{student.email}</p>*/}
-          </h2>
-        </div>
-      </div>
-      )
+    // if (students && students.length) {
+    //   students.map((student, id) => {
+    function displayLogic(){
+        // for (let i = 0; i < state.students.length; i++) {
+        //     toreturn.push(<h2><p>{state.students[i].firstName}</p></h2>);
+        // }
+        // });
+        //
+        // return toreturn;
+    }
+    if (state.students.length){
+        return (
+            <div>
+                <h1>Students</h1>
+                <div>
+                    {state.students.map((student) =>
+                        <h2><p>{student.firstName}
+                        {student.secondName}</p></h2>
+                    )}
+                </div>
+            </div>
+        )
+    }
+    else{
+        return ("No one")
+    }
 }
-
 export default App;
